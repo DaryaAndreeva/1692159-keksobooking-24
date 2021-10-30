d88868bfd4e800fd214cfa4467177fbf6d2d3f25
DaryaAndreeva <andreeva darya1987@gmail.com>
2021-10-31 00:47:01 +0300

    new functions

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+function getRandomFigure(a, b) {
+    min = Math.ceil(a);
+    max = Math.floor(b);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+  function getRandomArbitrary(a, b, c) {
+    return Math.round(Math.random() * ((b - a)*10**c)/10**c;
+  }
+ 
